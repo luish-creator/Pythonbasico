1b7da1b0be167b3c14fc4776506c6160b6b8c371
luish-creator <luishumbertorobles2009@gmail.com>
2026-02-11 16:17:11 -0800

    sasion 02 agregada

---
+++ sesion_02.py
@@ -0,0 +1,18 @@
+# Tuplas
+mi_tupla = (2,4)
+print("mi tupla",mi_tupla)
+
+# lista
+mi_lista = (1,3.1416, "luish",mi_tupla)
+print("el primer elemento de mi lista: ",mi_lista [0])
+print("el cuarto elemento de mi lista: ",mi_lista [3] )
+print("el tercer elemento de mi lista:",mi_lista [2])
+
+# Diccionario
+mi_diccionario = {"mi lista" : mi_lista,
+               "nombre": "luish",
+               "pi": 3.1416,
+               "tel": "644-19808"}
+print("llave para acceder a mi diccionario mi_lista",mi_diccionario ["mi lista"])
+print("llave para acceder a pi",mi_diccionario["pi"])
+print("llave para acceder a tel",mi_diccionario["tel"])
