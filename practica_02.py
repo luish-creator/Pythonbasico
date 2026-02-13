e4a88237823c1d382c8cc3c225107687bd7f6c57
luish-creator <luishumbertorobles2009@gmail.com>
2026-02-12 17:34:17 -0800

    guardado final de la clase

---
+++ practica_02.py
@@ -0,0 +1,9 @@
+# practica 2
+# dada la lista mi_lista_2 = [lunes, martes, miercoles, jueves, viernes]
+# imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# pista usa los dos tipos loops while y for rn el programa para lograrlo
+i=0
+mi_lista_2 = {"lunes","martes","miercoles","jueves","viernes"}
+
+for i in range(0,5):
+    print(mi_lista_2)
--- sesion_03.py
+++ sesion_03.py
@@ -19,3 +19,26 @@ mi_lista_2 = {"lunes","martes","miercoles","jueves","viernes"}
 for i in mi_lista_2:
     if i != "lunes" :
         print(f"feliz0 {i}")
+
+# while loop
+i=0
+
+while i < 5:
+    i+=1
+    if i == 3:
+        continue
+    print(i)
+    if i == 4:
+        break
+
+    else:
+        print("i es ahora mayor o igual a 5")
+
+# practica 2
+# dada la lista mi_lista_2 = [lunes, martes, miercoles, jueves, viernes]
+# imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# pista usa los dos tipos loops while y for rn el programa para lograrlo
+
+for i in range(i,9):
+    print(mi_lista_2)
+mi_lista_2 = {"lunes","martes","miercoles","jueves","viernes"}
