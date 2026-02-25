0406ca51eee5fa40fdf2b5429a01cc898f484aa2
luish-creator <luishumbertorobles2009@gmail.com>
2026-02-24 17:17:22 -0800

    clase Zombie

---
+++ OOP/Zombie.py
@@ -0,0 +1,11 @@
+from Enemigo import *
+
+class zombies(Enemigo):
+    def _init_(self,puntos_energia=10, ataque=1):
+        super()._init_(tipo_enemigo="Zombie", puntos_energia=puntos_energia,ataque=ataque)
+
+    def habla(self):
+        print("*hummm...*")
+
+    def propagar_enfermedad(self):
+        print("El zombie esta tratando de propagar la enfermedad!") 
